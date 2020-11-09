ce77112b3a58259884452b54151f3be0317bc920
Tom Early <n7tae@tearly.net>
2020-11-09 02:45:49 -0700

    roll back centos fix

--- callsignlistitem.cpp
+++ callsignlistitem.cpp
@@ -120,9 +120,7 @@ bool CCallsignListItem::CheckListedModules(const char *mods) const
 
 	// make sure every mods character is matched in m_Mods
 	const auto count = m_Mods.size();
-	bool found[count];
-	for (unsigned i=0; i<count; i++)
-		found[i] = false;
+	bool found[count] = { false };
 	for (auto p=mods; *p; p++)
 	{
 		auto pos = m_Mods.find(*p);
